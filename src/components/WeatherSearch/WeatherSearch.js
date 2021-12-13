76c94b6b92fa32cc3e2d4369ab56215878f763fa
CrowdedAstronaut <ross@johnrossphillips.com>
2021-12-13 14:56:13 -0600

    Added API key variable

--- src/components/WeatherSearch/WeatherSearch.js
+++ src/components/WeatherSearch/WeatherSearch.js
@@ -1,6 +1,77 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import WeatherSearchResults from "./WeatherSearchResults/WeatherSearchResults";
+import { Card } from "react-bootstrap";
 
 export default function WeatherSearch() {
-  return <WeatherSearchResults />;
+  const [weather, setWeather] = useState([]);
+  const [searchString, setSearchString] = useState("");
+
+  useEffect(() => {
+    geoWeather();
+  }, []);
+
+  async function geoWeather() {
+    await fetch("https://freegeoip.app/json/")
+      .then((res) => res.json())
+      .then((result) => {
+        setSearchString(result.zip_code);
+        getWeather();
+      });
+  }
+
+  const weatherSearch = {
+    key: process.env.REACT_APP_OPENWEATHER_API_KEY,
+    api: "https://api.openweathermap.org/data/2.5/weather?q=",
+    apikey: "&appid=",
+    units: "&units=imperial",
+  };
+
+  function handleChange(event) {
+    setSearchString(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    getWeather(searchString);
+    setSearchString("");
+  }
+
+  async function getWeather() {
+    const url = `${weatherSearch.api}${searchString},us${weatherSearch.units}${weatherSearch.apikey}${weatherSearch.key}`;
+
+    await fetch(url)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+      })
+      .catch(console.error);
+  }
+
+  return (
+    <Card className="weather-container">
+      <div className="weather-header">
+        <h1>Weather Depot</h1>
+      </div>
+
+      <div className="weather-form">
+        <form
+          onSubmit={handleSubmit}
+          className="search-form"
+        >
+          <input
+            placeholder="Search"
+            type="text"
+            name="searchString"
+            required
+            onChange={handleChange}
+            value={searchString}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+
+      <WeatherSearchResults weather={weather} />
+    </Card>
+  );
 }

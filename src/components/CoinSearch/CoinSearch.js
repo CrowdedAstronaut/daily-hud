4f034ace95607514681a7a334b49f4e2a2f3f330
CrowdedAstronaut <ross@johnrossphillips.com>
2021-12-21 21:43:12 -0600

    Hardcoded crypto dashboard

--- src/components/CoinSearch/CoinSearch.js
+++ src/components/CoinSearch/CoinSearch.js
@@ -3,6 +3,11 @@ import CoinSearchResults from "./CoinSearchResults/CoinSearchResults";
 import classes from "./CoinSearch.module.css";
 import LineChart from "react-linechart";
 import "../../../node_modules/react-linechart/dist/styles.css";
+import Bitcoin from "../../assets/icons/bitcoin-icon.svg";
+import Ethereum from "../../assets/icons/ethereum-icon.svg";
+import Cardano from "../../assets/icons/cardano_icon.svg";
+import TrendingUp from "../../assets/icons/coin-price-trending-up.svg";
+import TrendingDown from "../../assets/icons/coin-price-trending-down.svg";
 
 console.log("Hello from the Coin search component");
 export default function CoinSearch() {
@@ -43,20 +48,6 @@ export default function CoinSearch() {
     }
   }
 
-  const data = [
-    {
-      color: "red",
-      points: [
-        { x: 1, y: 7 },
-        { x: 2, y: -3 },
-        { x: 3, y: 5 },
-        { x: 4, y: -2 },
-        { x: 5, y: 6 },
-        { x: 6, y: -3 },
-      ],
-    },
-  ];
-
   return (
     <div className={classes["coin-search-container"]}>
       <div className={classes["coin-header"]}>
@@ -80,10 +71,55 @@ export default function CoinSearch() {
         <LineChart width={335} height={67} data={data} />
       </div> */}
       {/* <CoinSearchResults coins={coins} /> */}
-      <div className="coins_container">
-        <div className="coin_container bitcoin">Bitcoin</div>
-        <div className="coin_container ethereum">Ethereum</div>
-        <div className="coin_container cardano">Cardano</div>
+      <div className={classes["coins_container"]}>
+        <div className={classes["coin_container"]}>
+          <span>
+            <img src={Bitcoin} alt="" />
+            Bitcoin <br />
+            -1.43%
+          </span>
+          <div className="coin_mobile_chart">
+            <span>
+              <img src={TrendingUp} alt="" />
+            </span>
+          </div>
+          <div className="coin_price_info">
+            <span>46022.80</span>
+            <span>1 BTC</span>
+          </div>
+        </div>
+        <div className={classes["coin_container"]}>
+          <span>
+            <img src={Ethereum} alt="" />
+            Ethereum <br />
+            -1.43%
+          </span>
+          <div className="coin_mobile_chart">
+            <span>
+              <img src={TrendingDown} alt="" />
+            </span>
+          </div>
+          <div className="coin_price_info">
+            <span>4022.80</span>
+            <span>1 ETH</span>
+          </div>
+        </div>
+        <div className={classes["coin_container"]}>
+          <span>
+            <img src={Cardano} alt="" />
+            Cardano <br />
+            -1.43%
+          </span>
+          <div className="coin_mobile_chart">
+            <span>
+              <img src={TrendingUp} alt="" />
+            </span>
+          </div>
+          <div className="coin_price_info">
+            <span>1.02</span>
+            <span>1 ADA</span>
+          </div>
+        </div>
       </div>
     </div>
   );

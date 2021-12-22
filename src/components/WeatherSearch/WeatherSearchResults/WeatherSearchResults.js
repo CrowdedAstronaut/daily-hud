be007ccc62585f9292a59aab29678d2694452b2a
CrowdedAstronaut <ross@johnrossphillips.com>
2021-12-22 16:54:56 -0600

    Added all of the weather icons

--- src/components/WeatherSearch/WeatherSearchResults/WeatherSearchResults.js
+++ src/components/WeatherSearch/WeatherSearchResults/WeatherSearchResults.js
@@ -1,9 +1,10 @@
 import React from "react";
 import classes from "./WeatherSearchResults.module.css";
-import Sunrise from "../../../assets/icons/sunrise-icon.svg";
-import Sunset from "../../../assets/icons/sunset-icon.svg";
-import Humidity from "../../../assets/icons/humidity-icon.svg";
-import Clouds from "../../../assets/icons/cloud-icon.svg";
+import sunrise from "../../../assets/icons/sunrise-icon.svg";
+import sunset from "../../../assets/icons/sunset-icon.svg";
+import humidity from "../../../assets/icons/humidity-icon.svg";
+import clouds from "../../../assets/icons/cloud-icon.svg";
+import arrow from "../../../assets/icons/up-arrow-icon.svg";
 
 export default function WeatherSearchResults({ weather }) {
   if (weather.length === 0) {
@@ -17,29 +18,38 @@ export default function WeatherSearchResults({ weather }) {
             <br />
             <span>
               {" "}
-              <img src={Clouds} alt="" />
-              {weather.weather[0].description}
+              <img src={clouds} alt="" />
+              {weather.weather[0].description} <img src={humidity} alt="" />
+              {weather.main.humidity} %
             </span>
           </p>
 
           {/* <p>Low: {weather.main.temp_min}° F</p>
-          <p>High: {weather.main.temp_max}° F</p>
-          <p>Humidity: {weather.main.humidity}%</p> */}
+          
+          <p>humidity: {weather.main.humidity}%</p> */}
           <p>
-            <img src={Sunrise} alt="Sunrise Icon" />{" "}
-            {new Date(
-              weather.sys.sunrise * 1000
-            ).toLocaleTimeString("en-IN")}
+            <img src={sunrise} alt="sunrise Icon" />{" "}
+            {new Date(weather.sys.sunrise * 1000).toLocaleTimeString("en-IN")}
           </p>
           <p>
-            <img src={Sunset} alt="Sunset Icon" />{" "}
-            {new Date(
-              weather.sys.sunset * 1000
-            ).toLocaleTimeString("en-IN")}
+            <img src={sunset} alt="sunset Icon" />{" "}
+            {new Date(weather.sys.sunset * 1000).toLocaleTimeString("en-IN")}
           </p>
         </div>
         <div className={classes["temperature-container"]}>
           <h2> {weather.main.temp}° F</h2>
+          <p>
+            <img
+              src={arrow}
+              alt="low temperature"
+              style={{ transform: `rotate(${180}deg)` }}
+            />{" "}
+            {weather.main.temp_min}° F
+          </p>
+          <p>
+            <img src={arrow} alt="high temperature" />
+            {weather.main.temp_max}° F
+          </p>
         </div>
       </>
     );

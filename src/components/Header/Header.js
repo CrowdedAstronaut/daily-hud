63f7d6b716dfbabd3b6b9e4210e1323421bc9734
CrowdedAstronaut <ross@johnrossphillips.com>
2021-12-31 15:21:20 -0600

    Deployed site

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import classes from "./Header.module.css";
 export default function Header() {
   return (
     <div className={`${classes.header}`}>
-      <span>Hello Ross 👋</span>
+      <span>Hello Friend 👋</span>
       <h1> Welcome Back!</h1>
     </div>
   );
